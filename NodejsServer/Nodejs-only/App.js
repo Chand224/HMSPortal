afc1ac680e16797edad2b80f597819b72ffb4287
shyam chowdary <shyamchowdary@shyams-mbp.sby.ibm.com>
2018-04-17 10:38:40 -0400

    intial setup on express

--- NodejsServer/Nodejs-only/App.js
+++ NodejsServer/Nodejs-only/App.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const http=require('http');
 const login=require('./routes/Authentication-login');
+const cors = require('cors');
+
+var corsOptions = {
+    origin: 'http://localhost:5221',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
+
 
 // var server=http.createServer();
 var app=express();
 app.use(express.static(__dirname+'/../../FrontEnd/HMS-UI/dist'));
-
+app.use(cors());
 const port=process.env.port||5221;
 
 
@@ -29,6 +37,7 @@ function onListening(){
 }
 
 app.all('/login*',login);
+
 app.listen(port,()=>{
     console.log(`Port ${port} is open you can Configure it`);
 });
--- NodejsServer/Nodejs-only/routes/Authentication-login.js
+++ NodejsServer/Nodejs-only/routes/Authentication-login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var router=express.Router();
+const app=express();
 
 router.get('/login',(req,res,next)=>{
 res.send("get router file is excuted time god");
@@ -7,6 +8,10 @@ res.send("get router file is excuted time god");
 
 router.get('/login/signup',(req,res,next)=>{
 res.send("you are here to singup ++");
+});
+
+router.get('/login/check',(req,res,next)=>{
+    res.send({name:"Shyam"});
 });
 
 module.exports=router;
